0dc446337c0cfb94e7100ef3e7952f7365d303d7
AlenMujanovic <alen.mujanovicc@gmail.com>
2022-10-16 23:53:26 +0200

    Change Hash to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,20 +5,19 @@ import ProductDetailPage from "./components/ProductDetailPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { MainMenu } from "./components/MainMenu";
 import Cart from "./components/Cart";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <MainMenu></MainMenu>
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route path="/product/:pId" component={ProductDetailPage} />
-                <Route path="/cart" component={Cart} />
             </Switch>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
